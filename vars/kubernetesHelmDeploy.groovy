79b15d18323c5dbae0041f0b40735468d0fc45a3
rohith-marigowda <61956491+rohith-marigowda@users.noreply.github.com>
2023-12-21 00:55:46 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -11,8 +11,6 @@ def call (String dockerRegistry, String dockerImageTag, String helmChartName) {
     """
 
     sh """
-    helm create helm
-    helm install ${helmChartName} helm
     helm upgrade --install $helmChartName helm/ --set image.repository=$dockerRegistry:$dockerImageTag
     """
 }
