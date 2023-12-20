98eee015e5d1616ae4b9a7fee4e32b080ec3b60b
rohith-marigowda <61956491+rohith-marigowda@users.noreply.github.com>
2023-12-21 02:00:57 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -1,15 +1,14 @@
-def call (String dockerImageTag, String helmChartName) {
+def call (String dockerRegistry, String dockerImageTag, String helmChartName) {
     sh """
         if ! command -v helm > /dev/null; then
             echo "Helm not found. Installing Helm..."
             curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
             chmod 700 get_helm.sh
-            sudo ./get_helm.sh > /dev/null
+            ./get_helm.sh
             rm -f get_helm.sh
             echo "Helm installed successfully."
         fi
     """
-    sh 'helm upgrade --install $helmChartName ./helm/helm-deploy-sharedlibrary/ --set deployment.image="$dockerRegistry:$dockerImageTag" '
-    //sh "helm upgrade first --install $helmChartName ./helm/helm-deploy-sharedlibrary/ --set image.tag=$dockerImageTag"
+    
+    sh 'helm upgrade --install $helmChartName ./helm/helm-deploy-sharedlibrary/ --set image.repository="$dockerRegistry:$dockerImageTag" '
 }
-
