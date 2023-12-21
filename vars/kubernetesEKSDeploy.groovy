7374e9dbc4f66134e757f3fb10d3198f2580ff57
anusha1908 <142886569+anusha1908@users.noreply.github.com>
2023-12-21 21:47:22 +0530

    Update kubernetesEKSDeploy.groovy

--- vars/kubernetesEKSDeploy.groovy
+++ vars/kubernetesEKSDeploy.groovy
@@ -1,5 +1,28 @@
 def call (String dockerRegistry, String dockerImageTag, String kubernetesDeployment, String kubernetesContainer, String awsCredID, String awsRegion, String eksClusterName) {
-   
+    sh """
+        if ! command -v aws > /dev/null; then
+            echo "AWS CLI not found. Installing AWS CLI..."
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 2> /dev/null
+            
+            dpkg-query -l unzip 1>/dev/null || true
+            if [ \$? -ne 0 ]; then
+                echo "Unzip is not installed. Installing unzip..."
+                sudo apt update &> /dev/null
+                sudo apt -y install unzip &> /dev/null
+                echo "Unzip installed successfully."
+            fi
+            
+            unzip awscliv2.zip > /dev/null
+            sudo ./aws/install > /dev/null
+            rm -rf awscliv2.zip aws
+            echo "AWS CLI installed successfully."
+        fi
+        if ! command -v kubectl > /dev/null; then
+            curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+            sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+        fi
+    """
+
     withCredentials([usernamePassword(
         credentialsId: "$awsCredID",
         usernameVariable: "awsAccessKey",
@@ -14,11 +37,11 @@ def call (String dockerRegistry, String dockerImageTag, String kubernetesDeploym
 
                 kubectl get deploy $kubernetesDeployment || true; if [ \$? -ne 0 ]; then
                     echo "Updating image of deployment $kubernetesDeployment"
-                    kubectl set image deploy $kubernetesDeployment -n ingress-nginx $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
+                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
                 else
                     echo "Creating deployment $kubernetesDeployment from manifest file"
-                    kubectl apply -f deploymentserviceCluster.yaml -n ingress-nginx --record
-                    kubectl set image deploy $kubernetesDeployment -n ingress-nginx $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
+                    kubectl apply -f manifest.yml --record
+                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
                 fi
             """
         }
