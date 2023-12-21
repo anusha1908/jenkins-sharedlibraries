a16e63185e8ef5ee1592b74a7aad3a636a666c86
anusha1908 <142886569+anusha1908@users.noreply.github.com>
2023-12-21 23:19:57 +0530

    Update kubernetesEKSDeploy.groovy

--- vars/kubernetesEKSDeploy.groovy
+++ vars/kubernetesEKSDeploy.groovy
@@ -38,11 +38,11 @@ def call (String dockerRegistry, String dockerImageTag, String kubernetesDeploym
 
                 kubectl get deploy $kubernetesDeployment || true; if [ \$? -ne 0 ]; then
                     echo "Updating image of deployment $kubernetesDeployment"
-                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
+                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" 
                 else
                     echo "Creating deployment $kubernetesDeployment from manifest file"
                     kubectl apply -f manifest.yml --record
-                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" --record
+                    kubectl set image deploy $kubernetesDeployment $kubernetesContainer="$dockerRegistry:$dockerImageTag" 
                 fi
             """
         }
