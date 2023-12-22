0cb9db16c3054fa1b6c6dbfaa0d707adba9692c1
anusha1908 <142886569+anusha1908@users.noreply.github.com>
2023-12-22 09:57:12 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -2,7 +2,7 @@ def call (String dockerRegistry, String dockerImageTag, String helmChartName) {
     sh """
         if ! command -v helm > /dev/null; then
             echo "Helm not found. Installing Helm..."
-            sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+            sudo curl -fsSL -S -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
             sudo chmod 700 get_helm.sh
             sudo ./get_helm.sh 
             sudo rm -f get_helm.sh
