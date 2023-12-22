99e4c2f394fae2f93ec18dd174bc55bfdf45708a
anusha1908 <142886569+anusha1908@users.noreply.github.com>
2023-12-22 11:53:23 +0530

    Update kubernetesHelmDeploy.groovy

--- vars/kubernetesHelmDeploy.groovy
+++ vars/kubernetesHelmDeploy.groovy
@@ -9,7 +9,7 @@ def call (String dockerRegistry, String dockerImageTag, String helmChartName,Str
             echo "Helm installed successfully."
         fi
     """
-    sh "aws eks --region $awsRegion  update-kubeconfig --name $eksClusterName"
+    
    sh "helm upgrade --install $helmChartName helm/ --set image.repository=$dockerRegistry:$dockerImageTag"
 
 }
